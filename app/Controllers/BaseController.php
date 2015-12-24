c44d83910e7166fe02bf59fa89e70ec75422a3fa
deepak <deepak123shah@gmail.com>
2015-12-24 13:06:56 +0545

    access header

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -19,6 +19,7 @@ class BaseController
      */
     function __construct()
     {
+        //header('Access-Control-Allow-Origin: *');
         $this->request = Request::createFromGlobals();
     }
 
