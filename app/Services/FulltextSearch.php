ed82b15c41c232802491425512db1d3a2b25c3cd
Adina Petrean <adina_m3dy@yahoo.com>
2018-10-13 17:45:30 +0300

    #1145 Fixed error when invalid order passed as parameter

--- app/Services/FulltextSearch.php
+++ app/Services/FulltextSearch.php
@@ -154,19 +154,19 @@ class FulltextSearch extends Services
         ];
         if (isset($request['sort_by']) and !empty($request['sort_by'])) {
             if ($request['sort_by'] == "country") {
-                $params['body']['sort'][$lang.'.country_name']['order'] = (isset($request['order']) and !empty($request['order'])) ? $request['order'] : self::ORDER;
+                $params['body']['sort'][$lang.'.country_name']['order'] = $this->getSortOrder($request);
             }
             if ($request['sort_by'] == "year") {
-                $params['body']['sort'][$lang.'.signature_year']['order'] = (isset($request['order']) and !empty($request['order'])) ? $request['order'] : self::ORDER;
+                $params['body']['sort'][$lang.'.signature_year']['order'] = $this->getSortOrder($request);
             }
             if ($request['sort_by'] == "contract_name") {
-                $params['body']['sort'][$lang.'.contract_name.raw']['order'] = (isset($request['order']) and !empty($request['order'])) ? $request['order'] : self::ORDER;
+                $params['body']['sort'][$lang.'.contract_name.raw']['order'] = $this->getSortOrder($request);
             }
             if ($request['sort_by'] == "resource") {
-                $params['body']['sort'][$lang.'.resource_raw']['order'] = (isset($request['order']) and !empty($request['order'])) ? $request['order'] : self::ORDER;
+                $params['body']['sort'][$lang.'.resource_raw']['order'] = $this->getSortOrder($request);
             }
             if ($request['sort_by'] == "contract_type") {
-                $params['body']['sort'][$lang.'.contract_type']['order'] = (isset($request['order']) and !empty($request['order'])) ? $request['order'] : self::ORDER;
+                $params['body']['sort'][$lang.'.contract_type']['order'] = $this->getSortOrder($request);
             }
         }
 
@@ -565,6 +565,18 @@ class FulltextSearch extends Services
         }
 
         return $data;
+    }
+
+    /**
+     * @param $request
+     * @return string
+     */
+    private function getSortOrder($request)
+    {
+        return (isset($request['order']) and in_array(
+                $request['order'],
+                ['desc', 'asc']
+            )) ? $request['order'] : self::ORDER;
     }
 
 }
