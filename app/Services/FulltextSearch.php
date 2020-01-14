8e6e7ef918760d511904cbd5bf4d6f5fe165044e
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2020-01-14 13:08:32 +0545

    1250-sorting-bug
    * [x] Modified signature field to raw for sorting

--- app/Services/FulltextSearch.php
+++ app/Services/FulltextSearch.php
@@ -178,7 +178,7 @@ class FulltextSearch extends Services
                 $params['body']['sort'][$lang . '.country_name.keyword']['order'] = $this->getSortOrder($request);
             }
             if ($request['sort_by'] == "year") {
-                $params['body']['sort'][$lang . '.signature_year.keyword']['order'] = $this->getSortOrder($request);
+                $params['body']['sort'][$lang . '.signature_year.raw']['order'] = $this->getSortOrder($request);
             }
             if ($request['sort_by'] == "contract_name") {
                 $params['body']['sort'][$lang . '.contract_name.raw']['order'] = $this->getSortOrder($request);
